89963179ba77bfba7dcd140bec03b08620a899d5
PasaaYB <lananggading.pasa@gmail.com>
2025-12-23 08:59:11 +0700

    [feat - stock record]

--- backend/app/Http/Controllers/Warehouse/MaterialController.php
+++ backend/app/Http/Controllers/Warehouse/MaterialController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Warehouse;
 
+use App\Models\StockRecord;
 use Illuminate\Http\Request;
 use App\Models\Warehouse\Material;
 use App\Http\Controllers\Controller;
@@ -11,7 +12,16 @@ class MaterialController extends Controller
     public function index()
     {
         $materials = Material::all();
-
+        // dd($materials->toArray());
         return view('inventory.materials.index', compact('materials'));
+    }
+
+    public function stockRecords($id)
+    {
+        $stockRecords = StockRecord::with('material', 'material.ingredient')->where('material_id', $id)->get();
+
+        // dd($stockRecords->toArray());
+
+        return view('inventory.materials.stock_records', compact('stockRecords'));
     }
 }
